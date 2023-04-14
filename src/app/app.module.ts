20c74cb7ff7d770714e93d3bc078b23b0262b544
MatheusZag <zagrakalin@gmail.com>
2023-04-14 14:57:33 -0300

    Initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostComponent } from './components/post/post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MessagesService } from './services/messages.service';
+import { AvatarComponent } from './components/avatar/avatar.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    TweetComponent,
+    HomeComponent,
+    FooterComponent,
+    AvatarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    MessagesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
